9a1e3b055e899967de6a25e7d0511d10d87b47e1
TopMak <26412430+TopMak@users.noreply.github.com>
2018-08-01 02:14:54 +0300

    Teht4.2 refactor code more modular, add middlewares

--- index.js
+++ index.js
@@ -4,45 +4,28 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const blogsRouter = require('./controllers/blogs')
 
-module.exports = Blog
-
-app.use(cors())
-app.use(bodyParser.json())
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then()
+  .catch( err => console.log("DB_ERROR", err))
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.logger)
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.error)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

7aa86cd173871a7b2d9235a4b8739c44c0d2c4c9
TopMak <26412430+TopMak@users.noreply.github.com>
2018-08-02 12:26:30 +0300

    Teht4.4 and 4.5, 4.6 totalLikes and favouriteBlog tests, mostblogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,66 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  let initVal = 0 //Must provide init value when summing values in objects!!
+  return blogs.reduce( (acc, blog) => acc + blog.likes, initVal )
+
+}
+
+const favoriteBlog = (blogs) => {
+
+  let initVal = {"likes" : 0}
+
+  return blogs.reduce( (blogWithMostLikes, blog) => {
+    //console.log(blogWithMostLikes)
+    if(blogWithMostLikes.likes < blog.likes){
+      //Desctructor to remove some unnecessary values
+      ({_id, url, __v, ...cleanBlog} = blog)
+      blogWithMostLikes = cleanBlog
+    }
+    return blogWithMostLikes
+
+  }, {"likes" : 0})
+
+}
+
+const mostBlogs = (blogs) => {
+
+bloggerWithMost = blogs.reduce( (bloggersAndPosts, blog) =>{
+
+  //Using filter, but doesn't return index...
+  //let isBloggerOnList = bloggersAndPosts.filter( blogger => (blogger.author == blog.author) )
+  //if(isBloggerOnList.length == 0){
+
+  let bloggerOnList = bloggersAndPosts.findIndex(blogger => blogger.author == blog.author)
+
+  if(bloggerOnList === -1){
+    //Index not found, add blogger to array
+    bloggersAndPosts.push({
+      "author" : blog.author,
+      "blogs" : 1
+    })
+    //console.log("blogger added")
+  }
+  else {
+    //blogger is on the list, add blogs by 1
+    bloggersAndPosts[bloggerOnList].blogs += 1
+    //console.log("blogger's post count added'")
+  }
+
+  return bloggersAndPosts
+
+}, [])
+
+//returns the first blogger and blog count that has the highest blog count
+return bloggerWithMost.reduce( (max, x) => x.blogs > max.blogs ? x : max)
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
